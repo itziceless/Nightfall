f3124bfaa969055e55c6f4d73a534ee6de230de8
Iceless <xiceless@gmail.com>
2025-08-28 17:16:24 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,42 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 
-LicenseKeys = {
-    ["OWNER-Q4R7-T8Y2-U1I5"] = { type = "FOREVER", firstUse = nil },
-    ["ADMIN-P3L9-K2J6-M7N4"] = { type = "FOREVER", firstUse = nil },
-    ["ADMIN-A1B5-C8D2-E9F6"] = { type = "FOREVER", firstUse = nil },
-    ["PRIVATE-G7H3-J2K5-L8M1"] = { type = "FOREVER", firstUse = nil },
-    ["PRIVATE-X4Y9-Z1A7-B6C3"] = { type = "FOREVER", firstUse = nil },
-    ["PRIVATE-D2E8-F3G5-H1I9"] = { type = "FOREVER", firstUse = nil },
-    ["J6K4-L7M2-N9O1"] = { type = "1M", firstUse = nil },
-    ["P8Q3-R5S1-T6U7"] = { type = "FOREVER", firstUse = nil },
-    ["V2W9-X3Y6-Z8A4"] = { type = "1D", firstUse = nil },
-    ["B5C1-D7E2-F4G9"] = { type = "1W", firstUse = nil },
-    ["H3I8-J1K7-L2M5"] = { type = "1M", firstUse = nil },
-    ["N6O2-P4Q8-R1S3"] = { type = "FOREVER", firstUse = nil },
-    ["T7U3-V5W9-X2Y6"] = { type = "1D", firstUse = nil },
-    ["Z8A1-B3C6-D5E2"] = { type = "1W", firstUse = nil },
-    ["F4G7-H2I9-J6K3"] = { type = "1M", firstUse = nil },
-    ["L1M5-N8O2-P3Q7"] = { type = "FOREVER", firstUse = nil },
-    ["R9S4-T2U6-V1W5"] = { type = "1D", firstUse = nil },
-    ["X3Y7-Z5A9-B2C8"] = { type = "1W", firstUse = nil },
-    ["D6E1-F8G3-H4I7"] = { type = "1M", firstUse = nil },
-    ["J2K9-L5M1-N3O8"] = { type = "FOREVER", firstUse = nil },
-    ["P7Q4-R9S2-T5U1"] = { type = "1D", firstUse = nil },
-    ["V6W3-X8Y1-Z4A5"] = { type = "1W", firstUse = nil },
-    ["B9C2-D5E8-F1G4"] = { type = "1M", firstUse = nil },
-    ["H7I2-J4K6-L9M3"] = { type = "FOREVER", firstUse = nil },
-    ["N1O5-P8Q3-R6S2"] = { type = "1D", firstUse = nil },
-}
-
-for _, v in pairs(LicenseKeys) do 
-    if getgenv().CheckLicense == LicenseKeys then
-        whitelisted = true
-        break
-    end
-end
-     
-if whitelisted then
-	local isfile = isfile or function(file)
+local isfile = isfile or function(file)
     local suc, res = pcall(function()
         return readfile(file)
     end)
@@ -75,6 +39,42 @@ if not isfile('Galaxy/Games/Universal.lua') then
     writefile('Galaxy/Games/Universal.lua', loadstring(game:HttpGet("https://raw.githubusercontent.com/itziceless/Galaxy/refs/heads/main/Games/Universal.lua", true))())
 end
 
+local LicenseKeys = {
+    ["OWNER-Q4R7-T8Y2-U1I5"] = { type = "FOREVER", firstUse = nil },
+    ["ADMIN-P3L9-K2J6-M7N4"] = { type = "FOREVER", firstUse = nil },
+    ["ADMIN-A1B5-C8D2-E9F6"] = { type = "FOREVER", firstUse = nil },
+    ["PRIVATE-G7H3-J2K5-L8M1"] = { type = "FOREVER", firstUse = nil },
+    ["PRIVATE-X4Y9-Z1A7-B6C3"] = { type = "FOREVER", firstUse = nil },
+    ["PRIVATE-D2E8-F3G5-H1I9"] = { type = "FOREVER", firstUse = nil },
+    ["J6K4-L7M2-N9O1"] = { type = "1M", firstUse = nil },
+    ["P8Q3-R5S1-T6U7"] = { type = "FOREVER", firstUse = nil },
+    ["V2W9-X3Y6-Z8A4"] = { type = "1D", firstUse = nil },
+    ["B5C1-D7E2-F4G9"] = { type = "1W", firstUse = nil },
+    ["H3I8-J1K7-L2M5"] = { type = "1M", firstUse = nil },
+    ["N6O2-P4Q8-R1S3"] = { type = "FOREVER", firstUse = nil },
+    ["T7U3-V5W9-X2Y6"] = { type = "1D", firstUse = nil },
+    ["Z8A1-B3C6-D5E2"] = { type = "1W", firstUse = nil },
+    ["F4G7-H2I9-J6K3"] = { type = "1M", firstUse = nil },
+    ["L1M5-N8O2-P3Q7"] = { type = "FOREVER", firstUse = nil },
+    ["R9S4-T2U6-V1W5"] = { type = "1D", firstUse = nil },
+    ["X3Y7-Z5A9-B2C8"] = { type = "1W", firstUse = nil },
+    ["D6E1-F8G3-H4I7"] = { type = "1M", firstUse = nil },
+    ["J2K9-L5M1-N3O8"] = { type = "FOREVER", firstUse = nil },
+    ["P7Q4-R9S2-T5U1"] = { type = "1D", firstUse = nil },
+    ["V6W3-X8Y1-Z4A5"] = { type = "1W", firstUse = nil },
+    ["B9C2-D5E8-F1G4"] = { type = "1M", firstUse = nil },
+    ["H7I2-J4K6-L9M3"] = { type = "FOREVER", firstUse = nil },
+    ["N1O5-P8Q3-R6S2"] = { type = "1D", firstUse = nil },
+}
+
+for _, v in pairs(LicenseKeys) do 
+    if getgenv().CheckLicense == LicenseKeys then
+        whitelisted = true
+        break
+    end
+end
+     
+if whitelisted then
 loadfile('Galaxy/UI/Main.lua')
 loadfile('Galaxy/Games/Universal.lua')
 	else
