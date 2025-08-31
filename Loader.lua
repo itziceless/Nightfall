b58feb51aed7b09d0e9428fe0f980e72833b4be2
Iceless <xiceless@gmail.com>
2025-08-31 14:18:13 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,31 @@
 repeat task.wait() until game:IsLoaded()
 
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local delfile = delfile or function(file)
+	writefile(file, '')
+end
+
+local function wipeFolder(path)
+	if not isfolder(path) then return end
+	for _, file in listfiles(path) do
+		if file:find('loader') then continue end
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+			delfile(file)
+		end
+	end
+end
+
+for _, folder in {'Galaxy', 'Galaxy/Games', 'Galaxy/PremiumGames', 'Galaxy/Configs', 'Galaxy/Libs', 'Galaxy/UI'} do
+	if not isfolder(folder) then
+		makefolder(folder)
+	end
+end
+
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
@@ -23,10 +49,22 @@ local loadstring = function(...)
 	return res
 end
 
+	local commit = subbed:find('currentOid')
+	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+	commit = commit and #commit == 40 and commit or 'main'
+	if commit == 'main' or (isfile('Galaxy/Libs/commit.txt') and readfile('Galaxy/Libs/commit.txt') or '') ~= commit then
+		wipeFolder('Galaxy/Games')
+		wipeFolder('Galaxy/PremiumGames')
+		wipeFolder('newvape/UI')
+		wipeFolder('newvape/Libs')
+	end
+	writefile('Galaxy/Libs/commit.txt', commit)
+end
+
 if not isfile('Galaxy/UI/UI.txt') then
 	writefile('Galaxy/UI/UI.txt', 'Main')
 end
-local gui = readfile('newvape/UI/UI.txt')
+local gui = readfile('Galaxy/UI/UI.txt')
 
 local Galaxy = loadstring(downloadFile('Galaxy/UI/'..gui..'.lua'), 'gui')()
 shared.Galaxy = Galaxy
