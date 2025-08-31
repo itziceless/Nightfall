f9f553924c346c5992f08b7f667179491c1d0b9a
Iceless <xiceless@gmail.com>
2025-08-31 14:31:13 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -14,7 +14,7 @@ local function wipeFolder(path)
 	if not isfolder(path) then return end
 	for _, file in listfiles(path) do
 		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after Galaxy updates.')) == 1 then
 			delfile(file)
 		end
 	end
@@ -35,7 +35,7 @@ local function downloadFile(path, func)
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after Galaxy updates.\n'..res
 		end
 		writefile(path, res)
 	end
