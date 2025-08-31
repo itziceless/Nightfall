2a3491ff40f6f9d676d26320e54c615ead3977a4
Iceless <xiceless@gmail.com>
2025-08-31 13:58:29 -0400

    Update Universal.lua

--- PremiumGames/Universal.lua
+++ PremiumGames/Universal.lua
@@ -1,1 +1,29 @@
 API:CreateNotification('Galaxy', 'Welcome back to Galaxy Premium, '..userkey, 2)
+
+local playersService = cloneref(game:GetService('Players'))
+local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+local runService = cloneref(game:GetService('RunService'))
+local inputService = cloneref(game:GetService('UserInputService'))
+local tweenService = cloneref(game:GetService('TweenService'))
+local lightingService = cloneref(game:GetService('Lighting'))
+local marketplaceService = cloneref(game:GetService('MarketplaceService'))
+local teleportService = cloneref(game:GetService('TeleportService'))
+local httpService = cloneref(game:GetService('HttpService'))
+local guiService = cloneref(game:GetService('GuiService'))
+local groupService = cloneref(game:GetService('GroupService'))
+local textChatService = cloneref(game:GetService('TextChatService'))
+local contextService = cloneref(game:GetService('ContextActionService'))
+local coreGui = cloneref(game:GetService('CoreGui'))
+
+local isnetworkowner = identifyexecutor and table.find({'Zenith'}, ({identifyexecutor()})[1]) and isnetworkowner or function()
+	return true
+end
+local gameCamera = workspace.CurrentCamera or workspace:FindFirstChildWhichIsA('Camera')
+local lplr = playersService.LocalPlayer
+local assetfunction = getcustomasset
+
+local Galaxy = shared.Galaxy
+local tween = Galaxy.Libraries.tween
+local targetinfo = Galaxy.Libraries.targetinfo
+local getfontsize = Galaxy.Libraries.getfontsize
+local getcustomasset = Galaxy.Libraries.getcustomasset
