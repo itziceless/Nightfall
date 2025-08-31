14b22db8c34a7a668616f6b49f606204f1681d48
Iceless <xiceless@gmail.com>
2025-08-31 14:00:55 -0400

    Update Universal.lua

--- PremiumGames/Universal.lua
+++ PremiumGames/Universal.lua
@@ -1,4 +1,39 @@
 API:CreateNotification('Galaxy', 'Welcome back to Galaxy Premium, '..userkey, 2)
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		API:CreateNotification('Galaxy', 'Failed to load : '..err, 30)
+	end
+	return res
+end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/itziceless/Galaxy/'..readfile('Galaxy/Libs/commit.txt')..'/'..select(1, path:gsub('Galaxy/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+local run = function(func)
+	func()
+end
+local queue_on_teleport = queue_on_teleport or function() end
+local cloneref = cloneref or function(obj)
+	return obj
+end
 
 local playersService = cloneref(game:GetService('Players'))
 local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
