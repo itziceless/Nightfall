46e156c0ff4d2024904b5afb73df817e6ef7ff6b
Iceless <xiceless@gmail.com>
2025-09-05 18:16:34 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -71,7 +71,6 @@ end
 local gui = readfile('Galaxy/UI/UI.txt')
 
 local Galaxy = loadstring(downloadFile('Galaxy/UI/'..gui..'.lua'), 'gui')()
-print(Galaxy)
 loadstring(downloadFile('Galaxy/Games/Universal.lua'), 'Universal')()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/itziceless/Galaxy/refs/heads/main/libs/Whitelist.lua", true))()
 shared.Galaxy = Galaxy
