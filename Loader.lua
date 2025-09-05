261b61df5c5bac1d181b76ddde767ff02841c1fd
Iceless <xiceless@gmail.com>
2025-09-05 17:57:25 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -75,7 +75,7 @@ loadstring(downloadFile('Galaxy/Games/Universal.lua'), 'Universal')()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/itziceless/Galaxy/refs/heads/main/libs/Whitelist.lua", true))()
 shared.Galaxy = Galaxy
 
-print(shared.Galaxy)
+print(Galaxy)
 
 --[[local LicenseKeys = {
     "OWNER-Q4R7-T8Y2-U1I5",
