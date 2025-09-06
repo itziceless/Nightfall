d164abc35cc0c41409e7ef6ff9c1678dbfc63341
Iceless <xiceless@gmail.com>
2025-09-05 20:21:04 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -51,7 +51,7 @@ end
 
 if not shared.GalaxyDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/itziceless/Galaxy')
+		return game:HttpGet('https://raw.githubusercontent.com/itziceless/Galaxy/'..readfile('Galaxy/Libs/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
