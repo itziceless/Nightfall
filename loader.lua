4f7f3c78472ee943f0bafc1cd410b115c9c6ca21
Iceless <xiceless@gmail.com>
2025-09-08 14:23:00 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -20,7 +20,7 @@ local function wipeFolder(path)
 	end
 end
 
-for _, folder in {'Galaxy', 'Galaxy/Games', 'Galaxy/PremiumGames', 'Galaxy/Configs', 'Galaxy/Libs', 'Galaxy/UI'} do
+for _, folder in {'Revenant', 'Revenant/Games', 'Revenant/PremiumGames', 'Revenant/Configs', 'Revenant/Libs', 'Revenant/UI'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
@@ -29,7 +29,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/itziceless/Galaxy/'..readfile('Galaxy/Libs/commit.txt')..'/'..select(1, path:gsub('Galaxy/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/itziceless/Revenant/'..readfile('Revenant/Libs/commit.txt')..'/'..select(1, path:gsub('Revenant/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -44,43 +44,43 @@ end
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then
-		API:CreateNotification('Galaxy', 'Failed to load : '..err, 30)
+		API:CreateNotification('Revenant', 'Failed to load : '..err, 30)
 	end
 	return res
 end
 
-if not shared.GalaxyDeveloper then
+if not shared.RevenantDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://raw.githubusercontent.com/itziceless/Galaxy/'..readfile('Galaxy/Libs/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+		return game:HttpGet('https://raw.githubusercontent.com/itziceless/Revenant/'..readfile('Revenant/Libs/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('Galaxy/Libs/commit.txt') and readfile('Galaxy/Libs/commit.txt') or '') ~= commit then
-		wipeFolder('Galaxy/PremiumGames')
-		wipeFolder('Galaxy/Games')
-		wipeFolder('Galaxy/UI')
-		wipeFolder('Galaxy/Libs')
+	if commit == 'main' or (isfile('Revenant/Libs/commit.txt') and readfile('Revenant/Libs/commit.txt') or '') ~= commit then
+		wipeFolder('Revenant/PremiumGames')
+		wipeFolder('Revenant/Games')
+		wipeFolder('Revenant/UI')
+		wipeFolder('Revenant/Libs')
 	end
-	writefile('Galaxy/Libs/commit.txt', commit)
+	writefile('Revenant/Libs/commit.txt', commit)
 end
 
-if not isfile('Galaxy/UI/UI.txt') then
-	writefile('Galaxy/UI/UI.txt', 'Main')
+if not isfile('Revenant/UI/UI.txt') then
+	writefile('Revenant/UI/UI.txt', 'Main')
 end
-local gui = readfile('Galaxy/UI/UI.txt')
-loadstring(game:HttpGet("https://raw.githubusercontent.com/itziceless/Galaxy/refs/heads/main/libs/Whitelist.lua", true))()
-local Galaxy = loadstring(downloadFile('Galaxy/UI/'..gui..'.lua'), 'gui')()
-loadstring(downloadFile('Galaxy/Games/Universal.lua'), 'Universal')()
-	if isfile('Galaxy/Games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('Galaxy/Games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+local gui = readfile('Revenant/UI/UI.txt')
+loadstring(game:HttpGet("https://raw.githubusercontent.com/itziceless/Revenant/refs/heads/main/libs/Whitelist.lua", true))()
+local Revenant = loadstring(downloadFile('Revenant/UI/'..gui..'.lua'), 'gui')()
+loadstring(downloadFile('Revenant/Games/Universal.lua'), 'Universal')()
+	if isfile('Revenant/Games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('Revenant/Games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 	else
 		if not shared.VapeDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/itziceless/Galaxy/'..readfile('Galaxy/Libs/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet('https://raw.githubusercontent.com/itziceless/Revenant/'..readfile('Revenant/Libs/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('Galaxy/Games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+				loadstring(downloadFile('Revenant/Games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 			end
 		end
 	end
