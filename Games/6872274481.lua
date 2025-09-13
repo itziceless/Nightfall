8ddc9a10e3a0c8f4237238d992813733d326c049
Iceless <xiceless@gmail.com>
2025-09-12 22:10:20 -0400

    Update 6872274481.lua

--- Games/6872274481.lua
+++ Games/6872274481.lua
@@ -82,14 +82,10 @@ local entitylib = loadstring(
 )()
 
 local Client = require(replicatedStorage.TS.remotes).default.Client
-local KnitInit, Knit
-KnitInit, Knit = pcall(function()
-			return debug.getupvalue(require(lplr.PlayerScripts.TS.knit).setup, 6)
-		end)
-
-	--[[if not debug.getupvalue(Knit.Start, 1) then
+local Knit = debug.getupvalue(require(lplr.PlayerScripts.TS.knit).setup, 6)
+	if not debug.getupvalue(Knit.Start, 1) then
 		repeat task.wait() until debug.getupvalue(Knit.Start, 1)
-	end--]]
+	end
 local Flamework = require(replicatedStorage['rbxts_include']['node_modules']['@flamework'].core.out).Flamework
 local InventoryUtil = require(replicatedStorage.TS.inventory['inventory-util']).InventoryUtil
 local Bedwars = {
